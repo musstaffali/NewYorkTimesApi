cd540b6545d2e8a3c58fec349baa801140d06ab6
Mustafa Mustafa <mamustafa@gtcc.edu>
2021-01-30 22:22:41 -0500

    cleaning

--- NYTArticleSearch_Best_Solution/nytCode.js
+++ NYTArticleSearch_Best_Solution/nytCode.js
@@ -1,21 +1,13 @@
-/**
- * pulls information from the form and build the query URL
- * @returns {string} URL for NYT API based on form inputs
- */
 function buildQueryURL() {
-  // queryURL is the url we'll use to query the API
   var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
 
-  // Begin building an object to contain our API call's query parameters
-  // Set the API key
   var queryParams = { "api-key": "R1a31F4tBjCUaM2ho8GtIFsrSdtXt30M" };
 
-  // Grab text the user typed into the search input, add to the queryParams object
+
   queryParams.q = $("#search-term")
     .val()
     .trim();
 
-  // If the user provides a startYear, include it in the queryParams object
   var startYear = $("#start-year")
     .val()
     .trim();
